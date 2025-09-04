4cd7be72aaf1cf75efd8acc2a68e66aa1922567c
celeon09 <celeon95@gmail.com>
2025-09-03 22:07:48 -0600

    Implement code changes to enhance functionality and improve performance

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "f840189d49c73f27618e79dd57dd75b8
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6ad6899883195a6db0a17d826a5ec557",
+"flutter_bootstrap.js": "92f17cf2f7dcc39749215b1f64759902",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "42fc8957117a80e99136a11a81329d59",
 "/": "42fc8957117a80e99136a11a81329d59",
-"main.dart.js": "f8ff8ccf05852e510a4103454330c955",
+"main.dart.js": "f94db5f051572d1eeae175aa58b91c35",
 "manifest.json": "fea256752d57e7d0409ee54db10e0d75",
 "version.json": "d4f3a973ddddb51e5402582d0432c549"};
 // The application shell files that are downloaded before a service worker can
